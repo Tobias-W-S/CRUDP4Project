0903d6b5165de5cb9e075465c3a24ed86006e8b1
Tobias-W-S <94520362+Tobias-W-S@users.noreply.github.com>
2022-05-13 10:20:50 +0200

    Eerste Variant (#5)

--- index.php
+++ index.php
@@ -2,7 +2,21 @@
       include("includes/nav.php")
     ?>
 <div class="background_landing">
-    <div class="search_bar"></div>
+    <div class="search_bar">
+        
+        <form class="indexFilter" method="post" action="cruises.php">
+            <label>Vind uw perfecte cruise!</label>
+            <div>
+                <input type="text" placeholder="Bestemming..">
+                <input type="text" placeholder="Startpunt..">
+                <input type="text" placeholder="Duur..">
+                <input type="text" placeholder="Aantal personen..">
+                <input type="submit" value="Vind mijn cruise">
+            </div>    
+
+            
+        </form>
+    </div>
     <div class="cards"></div>
 </div>
 </body>
