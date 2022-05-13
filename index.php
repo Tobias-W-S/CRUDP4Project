c6f0dad069ea7f746a5f3f2db9a881729f74e524
amberopsteen <94449057+amberopsteen@users.noreply.github.com>
2022-05-13 10:27:57 +0200

    Amber (#6)
    * update
    
    * update
    
    * update index.php
    
    * update
    
    * test
    
    * update cards index.php
    
    Co-authored-by: Tobias-W-S <94520362+Tobias-W-S@users.noreply.github.com>
    Co-authored-by: Tobias-W-S <1196184@student.roc-nijmegen.nl>

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
       include("includes/nav.php")
     ?>
 <div class="background_landing">
+
     <div class="search_bar">
         
         <form class="indexFilter" method="post" action="cruises.php">
@@ -17,6 +18,76 @@
             
         </form>
     </div>
+
+</div>
+    <div class="cards">
+    <div class="card_box">
+        <div class="card">
+            <figure class="card__thumb">
+                <img src="media/cruise_ship.jpg" alt="" class="card__image">
+                <figcaption class="card__caption">
+                    <h2 class="card__title">populaire cruises</h2>
+                    <p class="card__snippet">In de winter op een van onze cruises? dan bent u bij ons bij het juiste adres</p>
+                    <a href="" class="card__button">Bekijk aanbod</a>
+                </figcaption>
+            </figure>
+        </div>
+	<div class="card">
+		<figure class="card__thumb">
+			<img src="media/seabourn.jpg" alt="" class="card__image">
+			<figcaption class="card__caption">
+				<h2 class="card__title">Zon vakantie</h2>
+				<p class="card__snippet">Heerlijk naar de zon op een van onze cruises? dan bent u bij ons bij het juiste adres</p>
+				<a href="" class="card__button">Bekijk aanbod</a>
+			</figcaption>
+		</figure>
+	</div>
+
+	<div class="card">
+		<figure class="card__thumb">
+			<img src="media/winter.jpg" alt="" class="card__image">
+			<figcaption class="card__caption">
+				<h2 class="card__title">Winter vakantie</h2>
+				<p class="card__snippet">In de winter op een van onze cruises? dan bent u bij ons bij het juiste adres</p>
+				<a href="" class="card__button">Bekijk aanbod</a>
+			</figcaption>
+		</figure>
+	</div>
+    <div class="card">
+		<figure class="card__thumb">
+			<img src="media/lastminute.jpg" alt="" class="card__image">
+			<figcaption class="card__caption">
+				<h2 class="card__title">Last minute</h2>
+				<p class="card__snippet">In de winter op een van onze cruises? dan bent u bij ons bij het juiste adres</p>
+				<a href="" class="card__button">Bekijk aanbod</a>
+			</figcaption>
+		</figure>
+	</div>
+    <div class="card">
+		<figure class="card__thumb">
+			<img src="media/europa.jpg" alt="" class="card__image">
+			<figcaption class="card__caption">
+				<h2 class="card__title">europa cruise</h2>
+				<p class="card__snippet">In de winter op een van onze cruises? dan bent u bij ons bij het juiste adres</p>
+				<a href="" class="card__button">Bekijk aanbod</a>
+			</figcaption>
+		</figure>
+	</div>
+    <div class="card">
+		<figure class="card__thumb">
+			<img src="media/zee.jpg" alt="" class="card__image">
+			<figcaption class="card__caption">
+				<h2 class="card__title">Middelandse zee cruises</h2>
+				<p class="card__snippet">In de winter op een van onze cruises? dan bent u bij ons bij het juiste adres</p>
+				<a href="" class="card__button">Bekijk aanbod</a>
+			</figcaption>
+		</figure>
+	</div>
+</div>
+    </div>
+
+
+
     <div class="cards"></div>
 </div>
 </body>
