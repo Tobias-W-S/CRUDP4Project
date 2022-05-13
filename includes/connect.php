19320a66584143c7579cc23086e1400ab00f3f97
amberopsteen <94449057+amberopsteen@users.noreply.github.com>
2022-05-13 08:39:58 +0200

    Amber (#4)
    * update
    
    * update
    
    * update index.php
    
    * update
    
    * test
    
    Co-authored-by: Tobias-W-S <94520362+Tobias-W-S@users.noreply.github.com>

---
+++ includes/connect.php
@@ -0,0 +1,24 @@
+<?php 
+
+$host = "localhost";
+$db = "crud_cruise";
+$user = "root";
+$pass = "";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $connect = new PDO($dsn, $user, $pass, $opt);
+
+} 
+catch (PDOException $e) 
+{
+    echo $e->getMessage();
+    die("Sorry, database probleem."); 
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+      include("includes/nav.php")
+    ?>
+<div class="background_landing">
+    <div class="search_bar"></div>
+    <div class="cards"></div>
+</div>
+</body>
+</html>
