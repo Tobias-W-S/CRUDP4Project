350b6dd23689815c22f6138307b8945cc509e361
amberopsteen <94449057+amberopsteen@users.noreply.github.com>
2022-05-20 09:40:13 +0200

    Cruises (#15)
    * update cruises
    
    * update index.php en cruises.php
    
    * update cruises
    
    * update cruises

---
+++ leesmeer_cruises.php
@@ -0,0 +1,32 @@
+<?php
+    include("includes/nav.php")
+?>
+<div class="background_leesmeer">
+    <div class="info_cruises"></div>
+    <div class="tab-wrap">
+        <input type="radio" name="tabs" id="tab1" checked>
+        <div class="tab-label-content" id="tab1-content">
+          <label for="tab1">Beschrijving</label>
+          <div class="tab-content">Bij deze beleving bent u verzekerd van een goede prijs en een comfortabele hut. Vaste diensten als alle maaltijden, havengelden en toegang tot de meeste faciliteiten aan boord zijn altijd inbegrepen. Tevens kunt u (indien beschikbaar) uw gewenste hutnummer zelf uitkiezen en uw voorkeur voor dinerzitting aangeven.</div>
+        </div>
+         
+        <input type="radio" name="tabs" id="tab2">
+        <div class="tab-label-content" id="tab2-content">
+          <label for="tab2">Prijzen</label>
+          <div class="tab-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laborum cumque maxime repudiandae obcaecati nisi suscipit sed voluptatem quaerat aliquid.</div>
+        </div>
+        
+        <input type="radio" name="tabs" id="tab3">
+        <div class="tab-label-content" id="tab3-content">
+          <label for="tab3">Weersverwachting</label>
+          <div class="tab-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident inventore voluptate perspiciatis quas recusandae quam cupiditate non repellat ut reiciendis?</div>
+        </div>
+      
+         <input type="radio" name="tabs" id="tab4">
+         <div class="tab-label-content" id="tab4-content">
+          <label for="tab4">Recenties</label>
+          <div class="tab-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, veritatis deserunt. Dolores perferendis vel corrupti voluptas reprehenderit ut quaerat delectus.</div>
+</div>
+</div>
+</body>
+</html>
