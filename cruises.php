7bc068a5510abcc9bf123604e99ba8186c49381b
Tobias-W-S <94520362+Tobias-W-S@users.noreply.github.com>
2022-05-25 14:20:54 +0200

    Cruise SQL Connect2 (#26)

--- cruises.php
+++ cruises.php
@@ -32,72 +32,14 @@
     echo '</div>';
     echo '<div class="product">';
     echo '<h2>' . $value['Naam'] . '</h2>';
-    echo '<h3>Vanaf €' . $value['Prijs'] . ',- per persoon</h3>';
+    echo '<h3>Vanaf €' . $value['Prijs'] . ' per persoon</h3>';
     echo '<p class="desc">' . $value['Beschrijving'] . '</p>';
     echo '<div class="buttons">';
-    echo '<button class="add">lees meer</button>';
+    echo '<a href="leesmeer_cruises.php?id=' . $value['reisID'] .'" id=' . $value['reisID'] . ' class="add">lees meer</a>';
     echo '</div>';
     echo '</div>';
     echo '</div>';
   }
 
-?>
-
-<?php 
-/*
-<div class="container">
-  <div class="images">
-    <img src="media/cruise1.jpg" />
-  </div>
-  <div class="product">
-    <h2>Norwegian Cruise Line</h2>
-    <h3>Vanaf 800,- per persoon</h3>
-    <p class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto quam dignissimos officiis natus aliquam laborum unde quasi. Quam, quidem. Consequuntur.</p>
-    <div class="buttons">
-    <button class="add">lees meer</button>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <div class="images">
-    <img src="media/cruise2.jpg" />
-  </div>
-  <div class="product">
-    <h2>Oceania Cruises</h2>
-    <h3>Vanaf 700,- per persoon</h3>
-    <p class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto quam dignissimos officiis natus aliquam laborum unde quasi. Quam, quidem. Consequuntur.</p>
-    <div class="buttons">
-    <button class='add' onclick="document.location='leesmeer_cruises.php'">lees meer</button>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <div class="images">
-    <img src="media/zee.jpg" />
-  </div>
-  <div class="product">
-    <h2>Royal Caribbean</h2>
-    <h3>Vanaf 900,- per persoon</h3>
-    <p class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto quam dignissimos officiis natus aliquam laborum unde quasi. Quam, quidem. Consequuntur.</p>
-    <div class="buttons">
-    <button class='add' onclick="document.location='leesmeer_cruises.php'">lees meer</button>
-    </div>
-  </div>
-</div>
-<div class="container">
-  <div class="images">
-    <img src="media/winter.jpg" />
-  </div>
-  <div class="product">
-    <h2>AIDA Cruises</h2>
-    <h3>Vanaf 750,- per persoon</h3>
-    <p class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto quam dignissimos officiis natus aliquam laborum unde quasi. Quam, quidem. Consequuntur.</p>
-    <div class="buttons">
-    <button class='add' onclick="document.location='leesmeer_cruises.php'">lees meer</button>
-    </div>
-  </div>
-</div>
-</div> 
-*/
 ?>
 
