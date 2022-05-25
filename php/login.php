b2dbfa58e3a58ad0ca336fa78cd161063a2df2d1
amberopsteen <94449057+amberopsteen@users.noreply.github.com>
2022-05-25 13:49:38 +0200

    Login css (#24)
    * login
    
    * update login pagina

--- php/login.php
+++ php/login.php
@@ -1,8 +1,3 @@
-<?php 
-
-    include('../includes/connect.php');
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css" />
 </head>
 <body>
+<?php 
+    include('../includes/connect.php');
+?>
+<div class="login_form">
     <form method="post" action="../includes/loginExecute.php">
+        <label>Gebruikersnaam</label>
         <input name="Gebruikersnaam" autocapitalize="none" type="email" class="lowercase" required>
+        <label>Wachtwoord</label>
         <input name="Wachtwoord" type="password" required>
         <input name="submitlogin" type="submit" value="Login">
     </form>
+</div>
     <script src="js/main.js"></script>
 </body>
 </html>
+
 
 
 
