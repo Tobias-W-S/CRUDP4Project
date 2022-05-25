e262c0ad9d3855eddb4cf5cf059738cfc1a004a5
amberopsteen <94449057+amberopsteen@users.noreply.github.com>
2022-05-25 12:30:29 +0200

    aanpassingen contact form (#20)
    Ik heb de contact pagina aangepast zodat het wat beter uitgelijnd is.

--- contact.php
+++ contact.php
@@ -1,28 +1,30 @@
 <?php
       include("includes/nav.php")
 ?>
-<div class="background_landing">
+<div class="background_contact">
     <div class="contact">
         <p>
             <strong>Contact</strong><br>
             <br>
             - Mail: fakfnanfla@gmail.com<br><br>
             - Telefoonnummer: 06 12345678<br><br>
-            - Adres: Een straat 101<br>
-            1234 AB Nijmegen<br>
+            - Adres: Een straat 101, 1234 AB Nijmegen<br>
         </p>
-        <div>
-            <form method="post">
-                <label>Mail: </label>
-                <input type="mail">
-                <label>Telefoonnummer: </label>
-                <input type="text">
-                <label>Uw bericht: </label>
-                <textarea></textarea>
-                <input type="submit">
-            </form>
-        </div>
     </div>
+    <div class="contact_form">
+        <form action="">
+          <label for="e-mail">Uw e-mail</label>
+          <input type="text" id="e-mail" name="e-mail" placeholder="Uw e-mailadres">
+      
+          <label for="onderwerp">Onderwerp</label>
+          <input type="text" id="onderwerp" name="onderwerp" placeholder="Typ hier het onderwerp">
+      
+          <label for="bericht">Uw bericht</label>
+          <textarea id="bericht" name="bericht" placeholder="Schrijf hier uw bericht" style="height:200px"></textarea>
+      
+          <input type="verzenden" value="Verzenden">
+        </form>
+      </div>
 </div>
 </body>
 </html>
