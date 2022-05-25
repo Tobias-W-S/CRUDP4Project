81b5a65898378705bde7c1cf44483aa522a68b6a
Tobias-W-S <1196184@student.roc-nijmegen.nl>
2022-05-25 13:44:21 +0200

    Cruise SQL Connect

--- cruises.php
+++ cruises.php
@@ -1,6 +1,6 @@
 <?php
   include("includes/nav.php");
-
+  include("includes/connect.php");
 
   $bestemming = "";
   $start = "";
@@ -13,13 +13,38 @@
     $duur = $_POST['Duur'];
     $personen = $_POST['Personen'];
   }
-  $sortname = "SELECT * FROM `cruises` WHERE `Eindbestemming` LIKE '" . $bestemming . "' AND `Startbestemming` LIKE ' " . $start . "' AND `Duur` LIKE '" . $duur . "' AND `Aantal personen` LIKE '" . $personen . "'"; 
+  $sql = "SELECT * FROM `cruises` WHERE `Eindbestemming` LIKE '%" . $bestemming . "%' AND `Startbestemming` LIKE '%" . $start . "%' AND `Duur` LIKE '%" . $duur . "%' AND `Aantal personen` LIKE '%" . $personen . "%'"; 
+  $stmt = $connect->prepare($sql);
+  $stmt->execute();
+  $result = $stmt->fetchAll();
 ?>
     <div class="cruises_titel">
       <h2>Kies hier uw cruise!</h2>
 
       </div>
 <div class="background_cruises">
+
+<?php 
+  foreach($result as $value){
+    echo '<div class="container">';
+    echo '<div class="images">';
+    echo '<img src="media/' . $value['Afbeelding'] . '" />';
+    echo '</div>';
+    echo '<div class="product">';
+    echo '<h2>' . $value['Naam'] . '</h2>';
+    echo '<h3>Vanaf €' . $value['Prijs'] . ',- per persoon</h3>';
+    echo '<p class="desc">' . $value['Beschrijving'] . '</p>';
+    echo '<div class="buttons">';
+    echo '<button class="add">lees meer</button>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+  }
+
+?>
+
+<?php 
+/*
 <div class="container">
   <div class="images">
     <img src="media/cruise1.jpg" />
@@ -29,7 +54,7 @@
     <h3>Vanaf 800,- per persoon</h3>
     <p class="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto quam dignissimos officiis natus aliquam laborum unde quasi. Quam, quidem. Consequuntur.</p>
     <div class="buttons">
-    <button class='add' onclick="document.location='leesmeer_cruises.php'">lees meer</button>
+    <button class="add">lees meer</button>
     </div>
   </div>
 </div>
@@ -72,4 +97,7 @@
     </div>
   </div>
 </div>
-</div>
+</div> 
+*/
+?>
+
