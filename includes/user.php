dada4da2768c61b2d48c7e06cc44de4505ebc59d
Tobias-W-S <94520362+Tobias-W-S@users.noreply.github.com>
2022-05-25 13:15:19 +0200

    Contactformulier verzenden (#22)

---
+++ includes/user.php
@@ -0,0 +1,14 @@
+<?php 
+
+    include("connect.php");
+
+    if(isset($_POST['CONTACT'])){
+        $sql =  "INSERT INTO `contact`(`Email`, `Onderwerp`, `Bericht`) 
+        VALUES (:Email, :Onderwerp, :Bericht)";
+        $stmt = $connect->prepare($sql);
+        $stmt->bindParam(":Email", $_POST['e-mail']);
+        $stmt->bindParam(":Onderwerp", $_POST['onderwerp']);
+        $stmt->bindParam(":Bericht", $_POST['bericht']);
+        $stmt->execute(); 
+        header("location: ../contact.php");
+    }
