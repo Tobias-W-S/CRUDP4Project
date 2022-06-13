ff402b79ece1966f3fe730f50ccd298594c7a5bf
Tobias-W-S <94520362+Tobias-W-S@users.noreply.github.com>
2022-06-13 13:43:05 +0200

    Recensies (#33)

---
+++ includes/review.php
@@ -0,0 +1,27 @@
+<input type="radio" name="tabs" id="tab5" />
+      <div class="tab-label-content" id="tab5-content">
+        <label for="tab5">Stuur review</label>
+        <div class="tab-content">
+          <form method="post" action="includes/user.php">
+            <input
+              type="text"
+              name="e-mail"
+              <?php echo'value="' . $_SESSION['user'] . '"'?>
+              readonly
+            />
+            <input
+              type="hidden"
+              name="reis"
+              <?php echo'value="' . $_GET['id'] . '"'?>
+              readonly
+            />
+            <textarea
+              type="text"
+              name="bericht"
+              placeholder="Schrijf hier uw recensie"
+              style="height: 100px"
+            ></textarea>
+            <input type="submit" name="RECENSIE" value="Verzenden" />
+          </form>
+        </div>
+      </div>
